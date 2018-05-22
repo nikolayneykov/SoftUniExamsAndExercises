5afee169afc87316468bd4334b10092375aa33a3
Nikolay <nikolayneykov92@abv.bg>
2018-05-22 21:21:50 +0200

    First Steps In Coding exercises

---
+++ ProgramingBasicsCSharp/Exercises/FirstStepsInCoding/04.TriangleOf55Stars/PrintTriangleOf55Stars.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _04.TriangleOf55Stars
+{
+    class PrintTriangleOf55Stars
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(new string('*', i));
+            }
+        }
+    }
+}
---
+++ ProgramingBasicsCSharp/Exercises/FirstStepsInCoding/06.SquareOfStars/PrintSquareOfStars.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _06.SquareOfStars
+{
+    class PrintSquareOfStars
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(new string('*', n));
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.WriteLine("*" + new string(' ', n - 2) + "*");
+            }
+            Console.WriteLine(new string('*', n));
+        }
+    }
+}
