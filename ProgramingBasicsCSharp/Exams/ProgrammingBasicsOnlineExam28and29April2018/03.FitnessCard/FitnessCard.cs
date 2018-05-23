87ea72e710b81d5ade47b9830cb292a2b9d2546f
Nikolay <nikolayneykov92@abv.bg>
2018-05-23 15:13:15 +0200

    Added Programming Basics Online Exam 28 and 29 April 2018

---
+++ ProgramingBasicsCSharp/Exams/ProgrammingBasicsOnlineExam28and29April2018/03.FitnessCard/FitnessCard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _03.FitnessCard
+{
+    class FitnessCard
+    {
+        static void Main(string[] args)
+        {
+            decimal availableMoney = decimal.Parse(Console.ReadLine());
+            char gender = char.Parse(Console.ReadLine());
+            int age = int.Parse(Console.ReadLine());
+            string sport = Console.ReadLine();
+
+            decimal price = 0m;
+            switch (sport)
+            {
+                case "Gym": price = gender == 'm' ? 42 : 35; break;
+                case "Boxing": price = gender == 'm' ? 41 : 37; break;
+                case "Yoga": price = gender == 'm' ? 45 : 42; break;
+                case "Zumba": price = gender == 'm' ? 34 : 31; break;
+                case "Dances": price = gender == 'm' ? 51 : 53; break;
+                case "Pilates": price = gender == 'm' ? 39 : 37; break;
+            }
+            price = age <= 19 ? price * 0.8m : price;
+            Console.WriteLine(price<= availableMoney? 
+                $"You purchased a 1 month pass for {sport}." : 
+                $"You don't have enough money! You need ${price-availableMoney:F2} more.");
+        }
+    }
+}
---
+++ ProgramingBasicsCSharp/Exams/ProgrammingBasicsOnlineExam28and29April2018/05.EiffelTower/EiffelTower.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _05.EiffelTower
+{
+    class EiffelTower
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(new string('-', n + 2) + "**" + new string('-', n + 2));
+            }
+            for (int i = 0; i < n - 3; i++)
+            {
+                Console.WriteLine(new string('-', n + 1) + "****" + new string('-', n + 1));
+            }
+            Console.WriteLine(new string('-', n) + "******" + new string('-', n));
+            for (int i = 0; i < n - 4; i++)
+            {
+                Console.WriteLine(new string('-', n) + "**--**" + new string('-', n));
+            }
+            for (int i = 0; i < n - 3; i++)
+            {
+                Console.WriteLine(new string('-', n - 1) + "**----**" + new string('-', n - 1));
+            }
+            Console.WriteLine(new string('-', n - 2) + "**********" + new string('-', n - 2));
+            int midCnt = 0;
+
+            for (int i = 0; i < n - 3; i++)
+            {
+                Console.WriteLine(new string('-', n - 3 - i) + "**" + new string('-', 8 + midCnt) + "**" + new string('-', n - 3 - i));
+                midCnt += 2;
+            }
+            Console.WriteLine("***" + new string('-', n * 2) + "***");
+
+        }
+    }
+}
