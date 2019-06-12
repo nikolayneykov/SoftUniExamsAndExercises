505f47c3b8fed8b9c254d6fa13442192a80a2ebe
Nikolay <nikolayneykov92@abv.bg>
2019-06-12 19:44:45 +0300

    Updated 03. Command Processor.js

--- JS-Core/JS Advanced/Lab Advanced Functions/03.CommandProcessor.js
+++ JS-Core/JS Advanced/Lab Advanced Functions/03.CommandProcessor.js
@@ -1,27 +1,17 @@
-function solve (params) {
-  let commandProcessor = (function () {
-    let data = ''
-
-    return {
-      append (str) {
-        data += str
-      },
-      removeStart (n) {
-        data = data.slice(n)
-      },
-      removeEnd (n) {
-        data = data.slice(0, data.length - n)
-      },
-      print () {
-        console.log(data)
-      }
+function solve () {
+  let data = ''
+  return {
+    append (str) {
+      data += str
+    },
+    removeStart (n) {
+      data = data.slice(n)
+    },
+    removeEnd (n) {
+      data = data.slice(0, data.length - n)
+    },
+    print () {
+      console.log(data)
     }
-  })()
-
-  params.forEach(param => {
-    let [command, arg] = param.split(' ')
-    commandProcessor[command](arg)
-  })
+  }
 }
-
-solve(['append hello', 'append again', 'removeStart 3', 'removeEnd 4', 'print'])
